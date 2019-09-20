a034e842ad9f834649f0e479614f6e6c0b41b104
Bryan Boreham <bjboreham@gmail.com>
2019-09-20 10:13:30 +0000

    Use Kubernetes V1 API instead of beta

--- pod.go
+++ pod.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"time"
 
-	appsv1 "k8s.io/api/apps/v1beta2"
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
